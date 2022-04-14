4e854979a2932cbcd2ae63e08beae7045c1afcea
HerryZheng <303983522@qq.com>
2022-04-14 17:28:35 +0800

    deploy

---
+++ precache-manifest.20b45e8b0074ec2e512b0fba0f501c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8801d6b7ba46378c4184",
+    "url": "/Cashbook/css/app.cd1f6562.css"
+  },
+  {
+    "revision": "641950f19c310ea3d07d828f17e46f30",
+    "url": "/Cashbook/index.html"
+  },
+  {
+    "revision": "8801d6b7ba46378c4184",
+    "url": "/Cashbook/js/app.03c79ffb.js"
+  },
+  {
+    "revision": "0fbfc2c67b4997a77bbb",
+    "url": "/Cashbook/js/chunk-vendors.2bd518c8.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Cashbook/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Cashbook/robots.txt"
+  }
+]);
